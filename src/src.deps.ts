6396e92cc3ccb333b73953418c691e60fd472dc2
michael.gearhardt@fathym.com <michael.gearhardt@fathym.com>
2023-08-18 07:06:30 +0000

    Refactor project structure and capitalize public methods in IoTEnsembleService

--- src/src.deps.ts
+++
@@ -1,7 +0,0 @@
-export * from "https://deno.land/x/fathym_atomic@v0.0.37-integration/mod.ts";
-export * from "https://deno.land/x/fathym_common@v0.0.90-eac/mod.ts";
-export {
-  type ComponentChildren,
-  type JSX,
-} from "https://esm.sh/preact@10.16.0";
-export { useEffect, useState } from "https://esm.sh/preact@10.16.0/hooks";
