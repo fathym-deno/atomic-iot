7aa593457e032ce47a6a8a9fb0518150e89d6ddb
mcgear <michael.gearhardt@gmail.com>
2023-10-04 07:38:30 -0600

    formatting and ref cleanup

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -1,7 +1,4 @@
-export * from "https://deno.land/x/fathym_atomic@v0.0.37-integration/mod.ts";
-export * from "https://deno.land/x/fathym_common@v0.0.90-eac/mod.ts";
-export {
-  type ComponentChildren,
-  type JSX,
-} from "https://esm.sh/preact@10.16.0";
-export { useEffect, useState } from "https://esm.sh/preact@10.16.0/hooks";
+export * from "https://deno.land/x/fathym_atomic@v0.0.84-integration/mod.ts";
+export * from "https://deno.land/x/fathym_common@v0.0.99/mod.ts";
+export { type ComponentChildren, type JSX } from "preact";
+export { useEffect, useState } from "preact/hooks";
